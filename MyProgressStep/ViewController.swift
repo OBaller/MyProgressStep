e17817c32cf653d971a117a422f96e76d8d9a4b1
OBaller <naseemoyebola@gmail.com>
2022-04-05 11:25:32 +0100

    StepperView

--- MyProgressStep/ViewController.swift
+++ MyProgressStep/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var steps = [StepperData]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        steps = createData()
+        let stepper = StepperView(frame: self.view.frame, steps: steps)
+        try? stepper.configureView(mode:
+                . vertical)
+        self.view.addSubview(stepper)
     }
 
-
+    
 }
 
